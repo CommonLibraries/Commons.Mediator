5027423c678f38c598e98431bd939125ce3a0a33
Truong Chau Hien <truongchauhien@gmail.com>
2023-03-30 20:35:25 +0700

    Add interfaces,

---
+++ Commons.Mediator/IMediator.cs
@@ -0,0 +1,14 @@
+﻿using Commons.Mediator.Notifications;
+using Commons.Mediator.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commons.Mediator
+{
+    public interface IMediator : IRequestDispatcher, INotificationDispatcher
+    {
+    }
+}
